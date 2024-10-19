02e104b3ae6c755216687d588fba15df1d9244ec
Kenm <kenm4115@gmail.com>
2024-10-19 15:18:23 +0300

    Modified files in Intro to DB

--- task_4.sql
+++ task_4.sql
@@ -1,15 +1,15 @@
 
-SELECT 
-    COLUMN_NAME, 
-    DATA_TYPE, 
-    IS_NULLABLE, 
-    COLUMN_DEFAULT, 
-    CHARACTER_MAXIMUM_LENGTH 
-FROM 
-    INFORMATION_SCHEMA.COLUMNS 
-WHERE 
-    TABLE_NAME = 'Books' 
-    AND TABLE_SCHEMA = 'alx_book_store';
+SELECT * FROM alx_book_store.books;
+--     COLUMN_NAME, 
+--     DATA_TYPE, 
+--     IS_NULLABLE, 
+--     COLUMN_DEFAULT, 
+--     CHARACTER_MAXIMUM_LENGTH 
+-- FROM 
+--     INFORMATION_SCHEMA.COLUMNS 
+-- WHERE 
+--     TABLE_NAME = 'Books' 
+--     AND TABLE_SCHEMA = 'alx_book_store';
 
 
 /*
