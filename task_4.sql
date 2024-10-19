853992c7eea0fd9e866de643db1f31ee031d0b19
Kenm <kenm4115@gmail.com>
2024-10-19 19:57:02 +0300

    Modified task 4

--- task_4.sql
+++ task_4.sql
@@ -4,17 +4,17 @@
 USE alx_book_store;
 
 -- Retrieve detailed information about the Books table
-SELECT * FROM alx_book_store.books
---     COLUMN_NAME,
---     DATA_TYPE,
---     CHARACTER_MAXIMUM_LENGTH AS max_length,
---     IS_NULLABLE,
---     COLUMN_DEFAULT
--- FROM 
---     information_schema.COLUMNS
--- WHERE 
---     TABLE_NAME = 'Books'
---     AND TABLE_SCHEMA = 'alx_book_store';
+SELECT
+    COLUMN_NAME,
+    DATA_TYPE,
+    CHARACTER_MAXIMUM_LENGTH AS max_length,
+    IS_NULLABLE,
+    COLUMN_DEFAULT
+FROM 
+    information_schema.COLUMNS
+WHERE 
+    TABLE_NAME = 'Books'
+    AND TABLE_SCHEMA = 'alx_book_store';
 
 
 
