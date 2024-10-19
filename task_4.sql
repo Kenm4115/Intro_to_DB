943b4057b15e231bfdfbd8ca7de7d1b947a5c5db
Kenm <kenm4115@gmail.com>
2024-10-19 18:50:14 +0300

    Modified task 4

--- task_4.sql
+++ task_4.sql
@@ -15,20 +15,38 @@
 --     TABLE_SCHEMA = DATABASE();
 
 
+-- USE alx_book_store;
+
+-- -- Retrieve full description of the Books table
+-- SELECT 
+--     COLUMN_NAME AS 'Column Name',
+--     DATA_TYPE AS 'Data Type',
+--     CHARACTER_MAXIMUM_LENGTH AS 'Max Length',
+--     IS_NULLABLE AS 'Is Nullable',
+--     COLUMN_DEFAULT AS 'Default Value'
+-- FROM 
+--     information_schema.COLUMNS
+-- WHERE 
+--     TABLE_NAME = 'Books' 
+--     AND TABLE_SCHEMA = DATABASE();
+
+
+-- Use the database passed as an argument
 USE alx_book_store;
 
--- Retrieve full description of the Books table
+-- Retrieve detailed information about the Books table
 SELECT 
-    COLUMN_NAME AS 'Column Name',
-    DATA_TYPE AS 'Data Type',
-    CHARACTER_MAXIMUM_LENGTH AS 'Max Length',
-    IS_NULLABLE AS 'Is Nullable',
-    COLUMN_DEFAULT AS 'Default Value'
+    COLUMN_NAME,
+    DATA_TYPE,
+    CHARACTER_MAXIMUM_LENGTH AS max_length,
+    IS_NULLABLE,
+    COLUMN_DEFAULT
 FROM 
     information_schema.COLUMNS
 WHERE 
-    TABLE_NAME = 'Books' 
-    AND TABLE_SCHEMA = DATABASE();
+    TABLE_NAME = 'Books'
+    AND TABLE_SCHEMA = 'alx_book_store';
+
 
 
 /*
