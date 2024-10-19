2338565c78bd219e1923da72458e93a5913bb80e
Kenm <kenm4115@gmail.com>
2024-10-19 19:51:57 +0300

    Modified task 4

--- task_4.sql
+++ task_4.sql
@@ -22,7 +22,7 @@ SELECT * FROM alx_book_store.books
 Write a script that prints the full description of the table books from the database alx_book_store in your MySQL server.
 
 The database name will be passed as an argument of the mysql command
-You are not allowed to use the DESCRIBE or EXPLAIN statements
+You are not allowed to use the describe or explain statements
 The name of the file should be task_4.sql
 All SQL keywords should be in uppercase
 
